0a339a8eb636549ee12e1b38887e5ff96e54b555
selenoida <selenaeillen@hotmail.com>
2024-08-27 19:29:02 -0300

    terminado

--- eventos.js
+++ eventos.js
@@ -1,3 +1,21 @@
-function saludar() {
+document.addEventListener("DOMContentLoaded", function() {
+    
+    let boton = document.getElementById("botonSaludar");
+
+    function saludar() {
     alert("Hola!");
+    }
+
+    boton.addEventListener("click", saludar); 
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+
+let soyEldiv = document.getElementById("elDiv");
+
+function saludaEldiv() {
+    alert("Hola! Soy el div");
 }
+
+soyEldiv.addEventListener("click", saludaEldiv);
+}) 
